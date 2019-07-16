f35b94be32d8002d6a40fe82a7db9a112252f088
Circular-test <nmartinez@corpit-consulting.com>
2019-07-16 16:41:33 -0300

    fix main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 resource "aws_lambda_function" "test_lambda" {
-  function_name = "${var.function_name}"
-  role          = "${var.role}"
-  handler       = "${var.handler}"
-  memory_size   = "${var.memory_size}"
-  description   = "${var.description}"
-  timeout       = "${var.timeout}"
-  s3_bucket     = "${var.s3_bucket}"
-  s3_key        = "${var.s3_key}"
+  function_name     = "${var.function_name}"
+  role              = "${var.role}"
+  handler           = "${var.handler}"
+  memory_size       = "${var.memory_size}"
+  description       = "${var.description}"
+  timeout           = "${var.timeout}"
+  s3_bucket         = "${var.s3_bucket}"
+  s3_key            = "${var.s3_key}"
+  s3_object_version = "${var.s3_object_version}"
 
     # The filebase64sha256() function is available in Terraform 0.11.12 and later
     # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
