c968174283b67c5e9fdd34655c3011a43477ac72
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-06-17 16:49:55 -0300

    new modulo for lambda on tf12

--- main.tf
+++ main.tf
@@ -1,72 +1,54 @@
 resource "aws_lambda_function" "test_lambda_1" {
-  count             = "${var.has_variables ? 1 : 0}"
-  function_name     = "${var.function_name}"
-  layers            = ["${var.layers}"]
-  role              = "${var.role}"
-  handler           = "${var.handler}"
-  memory_size       = "${var.memory_size}"
-  description       = "${var.description}"
-  timeout           = "${var.timeout}"
-  s3_bucket         = "${var.s3_bucket}"
-  s3_key            = "${var.s3_key}"
-  s3_object_version = "${var.s3_object_version}"
-  reserved_concurrent_executions= "${var.reserved_concurrent_executions}"
+  function_name                  = var.function_name
+  layers                         = var.layers
+  role                           = var.role
+  handler                        = var.handler
+  memory_size                    = var.memory_size
+  description                    = var.description
+  timeout                        = var.timeout
+  s3_bucket                      = var.s3_bucket
+  s3_key                         = var.s3_key
+  s3_object_version              = var.s3_object_version
+  reserved_concurrent_executions = var.reserved_concurrent_executions
 
-    # The filebase64sha256() function is available in Terraform 0.11.12 and later
-    # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-    # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
- 
-  source_code_hash   = "${var.source_code_hash}"
-  runtime            = "${var.runtime}"
-  environment        {
-    variables = "${var.variables}"
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
+
+  source_code_hash = var.source_code_hash
+  runtime          = var.runtime
+
+### Env Lambas
+   dynamic "environment" {
+    for_each = local.environment_map
+    content {
+      variables = environment.value
+    }
   }
-  vpc_config         {
-    subnet_ids         = ["${var.subnet_ids}"]
-    security_group_ids = ["${var.security_group_ids}"]
+
+####vpc Config
+
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = var.security_group_ids
   }
-  tracing_config     {
-    mode = "${var.mode}"
+  tracing_config {
+    mode = var.mode
   }
-  dead_letter_config = "${var.dead_letter_config}"
+  dynamic "dead_letter_config" {
+    for_each = var.dead_letter_config
+    content {
+      # TF-UPGRADE-TODO: The automatic upgrade tool can't predict
+      # which keys might be set in maps assigned here, so it has
+      # produced a comprehensive set here. Consider simplifying
+      # this after confirming which keys can be set in practice.
+
+      target_arn = dead_letter_config.value.target_arn
+    }
+  }
+
   lifecycle {
-    ignore_changes     = ["last_modified"]
+    ignore_changes = [last_modified]
   }
 }
 
-
-resource "aws_lambda_function" "test_lambda_2" {
-  count             = "${var.has_variables ? 0 : 1}"
-  function_name     = "${var.function_name}"
-  layers            = ["${var.layers}"]
-  role              = "${var.role}"
-  handler           = "${var.handler}"
-  memory_size       = "${var.memory_size}"
-  description       = "${var.description}"
-  timeout           = "${var.timeout}"
-  s3_bucket         = "${var.s3_bucket}"
-  s3_key            = "${var.s3_key}"
-  s3_object_version = "${var.s3_object_version}"
-  reserved_concurrent_executions = "${var.reserved_concurrent_executions}"
-
-    # The filebase64sha256() function is available in Terraform 0.11.12 and later
-    # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-    # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
- 
-  source_code_hash   = "${var.source_code_hash}"
-  runtime            = "${var.runtime}"
-  vpc_config         {
-    subnet_ids         = ["${var.subnet_ids}"]
-    security_group_ids = ["${var.security_group_ids}"]
-  }
-  tracing_config     {
-    mode = "${var.mode}"
-  }
-  dead_letter_config = "${var.dead_letter_config}" 
-
-### The lifecycle block was used because we use environment variables for s3_object_version // it is not mandatory to use this block, simply define the s3_object_version variable if you do not want to use lifecycle
-
-  lifecycle {
-    ignore_changes     = ["last_modified"]
-  }
-}
--- output.tf
+++ output.tf
@@ -1,23 +1,54 @@
 output "arn" {
   description = "The Amazon Resource Name (ARN) identifying your Lambda Function."
-  value = "${element(coalescelist(aws_lambda_function.test_lambda_1.*.arn, aws_lambda_function.test_lambda_2.*.arn),0)}"
+  value = element(
+    coalescelist(
+      aws_lambda_function.test_lambda_1.*.arn,
+      aws_lambda_function.test_lambda_2.*.arn,
+    ),
+    0,
+  )
 }
 
 output "id" {
   description = "The Amazon Resource Name (ID) identifying your Lambda Function."
-  value = "${element(coalescelist(aws_lambda_function.test_lambda_1.*.id, aws_lambda_function.test_lambda_2.*.id),0)}"
+  value = element(
+    coalescelist(
+      aws_lambda_function.test_lambda_1.*.id,
+      aws_lambda_function.test_lambda_2.*.id,
+    ),
+    0,
+  )
 }
 
 output "version" {
   description = "Latest published version of your Lambda Function."
-  value = "${element(coalescelist(aws_lambda_function.test_lambda_1.*.version, aws_lambda_function.test_lambda_2.*.version),0)}"
+  value = element(
+    coalescelist(
+      aws_lambda_function.test_lambda_1.*.version,
+      aws_lambda_function.test_lambda_2.*.version,
+    ),
+    0,
+  )
 }
 
 output "function_name" {
   description = "The Amazon Resource Name (function name) identifying your Lambda Function."
-  value = "${element(coalescelist(aws_lambda_function.test_lambda_1.*.function_name, aws_lambda_function.test_lambda_2.*.function_name),0)}"
+  value = element(
+    coalescelist(
+      aws_lambda_function.test_lambda_1.*.function_name,
+      aws_lambda_function.test_lambda_2.*.function_name,
+    ),
+    0,
+  )
 }
 
 output "s3_object_version" {
-  value = "${element(coalescelist(aws_lambda_function.test_lambda_1.*.s3_object_version, aws_lambda_function.test_lambda_2.*.s3_object_version),0)}"
+  value = element(
+    coalescelist(
+      aws_lambda_function.test_lambda_1.*.s3_object_version,
+      aws_lambda_function.test_lambda_2.*.s3_object_version,
+    ),
+    0,
+  )
 }
+
